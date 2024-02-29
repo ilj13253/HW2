5738935c2e182907f65e85efa16ac0516d7bca50
ilj13253 <butackovilj25@gmail.com>
2024-02-29 15:00:33 +0800

    hww

---
+++ System_prog/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace System_prog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(380, 81);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Result.Location = new System.Drawing.Point(167, 277);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(104, 32);
+            this.Result.TabIndex = 2;
+            this.Result.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Result;
+    }
+}
+
---
+++ System_prog/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+namespace System_prog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        delegate void UpdateResultCallback(string message);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(CountWords);
+            t.Start(textBox1.Text);
+        }
+        private void UpdateResult(string message)
+        {
+            Result.Text = message;
+        }
+        private void CountWords(object data)
+        {
+            var path = data as string;
+            try
+            {
+                var text = File.ReadAllText(path);
+                var words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string message = $"Result: {words.Length} words";
+                if (Result.InvokeRequired)
+                {
+                    UpdateResultCallback callback = new UpdateResultCallback(UpdateResult);
+                    Result.Invoke(callback, new object[] { message });
+                }
+                else
+                {
+                    UpdateResult(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = $"Error reading file: {ex.Message}";
+                if (Result.InvokeRequired)
+                {
+                    UpdateResultCallback callback = new UpdateResultCallback(UpdateResult);
+                    Result.Invoke(callback, new object[] { errorMessage });
+                }
+                else
+                {
+                    UpdateResult(errorMessage);
+                }
+            }
+        }
+    }
+}
